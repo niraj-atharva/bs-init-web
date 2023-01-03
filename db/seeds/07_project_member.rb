d9b0b65cbd7a24c864ec37d63cc16b2db2ee300a
kuldeep jadeja <kuldeep.ror.expert@gmail.com>
2023-01-03 12:20:16 +0530

    removing unused seeds

--- db/seeds/07_project_member.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Projects Members Start
-[@u5].each do |user|
-  user.project_members.create(project_id: @p1.id, hourly_rate: 1000)
-end
-
-puts "Projects Members Created"
-# Projects Members end
