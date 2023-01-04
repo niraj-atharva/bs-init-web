e836143c1af72f3a81cc1073c86e6c548bab764e
Niraj <niraj.virani@atharvasystem.com>
2023-01-04 21:20:41 +0530

    Revert "Merry Christmas"
    This reverts commit c2b3c2b034e5344c5a1f5c66372de101c2c645d7.

--- app/javascript/src/components/SpaceUsages/EditEntry.tsx
+++ app/javascript/src/components/SpaceUsages/EditEntry.tsx
@@ -221,7 +221,7 @@ const EditEntry: React.FC<Iprops> = ({
             <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
             <span className="sr-only">Close modal</span>
           </button>
-          {editEntryId === 0 ? (<div className="w-full p-6" style={{ backgroundColor: "#e55f64" }}>
+          {editEntryId === 0 ? (<div className="w-full p-6" style={{ backgroundColor: "#335CD6" }}>
             <p className="px-6 text-xl font-medium text-white">
               {`${getNumberWithOrdinal(selectedDateInfo["date"])} ${selectedDateInfo["month"]}, ${selectedDateInfo["year"]}`}
             </p>
