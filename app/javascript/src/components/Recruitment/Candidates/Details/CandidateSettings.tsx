b004befe28d1e3afc8d6d65b9bcb0dec89ba9ede
Abhishek-atharva <abhishek.react.expert@gmail.com>
2023-02-01 17:06:20 +0530

    #51 :[CANDIDATE] Fixes the Candidate UI Should match the theme.

--- app/javascript/src/components/Recruitment/Candidates/Details/CandidateSettings.tsx
+++ app/javascript/src/components/Recruitment/Candidates/Details/CandidateSettings.tsx
@@ -8,6 +8,7 @@ import candidateItemsApi from "apis/candidate-items";
 import candidates from "apis/candidates";
 
 import { unmapCandidateList } from "../../../../mapper/candidate.mapper";
+import { X } from "phosphor-react";
 
 const getInitialvalues = (candidateI) => ({
   assignee_id: candidateI.assignee_id,
@@ -87,87 +88,80 @@ const CandidateSettings = ({ candidateDetails, setCandidateDetails, setShowCandi
 
   return (
     <>
-      <span className='absolute top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50' />
-      <div aria-hidden="true" className="fixed top-0 left-0 right-0 z-50 w-full overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
-        <div className="relative float-right w-full h-full max-w-md md:h-auto">
-          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <div className="flex items-center justify-between w-full px-8 py-6" style={{ backgroundColor: "#335CD6" }}>
-              <strong className="text-xl font-medium text-white dark:text-white">Candidate Settings</strong>
-              <button type="button" className="inline-flex items-center text-sm text-gray-400 bg-transparent rounded-lg hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-white p-1.5"
-                onClick={() => {setShowCandidateSetting(false)}}>
-                <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                <span className="sr-only">Close modal</span>
-              </button>
-            </div>
-            <div className="px-6 py-6 lg:px-8">
-              <div className="space-y-6">
+<div className="sidebar__container flex flex-col p-6 justify-between overflow-auto">
+      <div>
 
-                <Formik
-                  initialValues={getInitialvalues(candidateDetails)}
-                  enableReinitialize={true}
-                  onSubmit={handleSubmit}
-                >
-                  {({ errors, touched }) => (
-                    <Form>
-                      <div className="mb-6">
-                        <label className="block mb-2 text-sm text-gray-600">Assignee</label>
-                        <select
-                          defaultValue={candidateDetails.assignee_id}
-                          className="w-full px-3 py-2 border border-gray-300 rounded-md bg-miru-gray-100 focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
-                          name="assignee_id" onChange={(e) => changeAssignee(e.target.value)}>
-                          <option value=''>Select Assignee</option>
-                          {allowUserList &&
-                            allowUserList.map((e: any) => <option value={e.id} selected={e.id === candidateDetails.assignee_id} key={e.id}>{e.full_name}</option>)}
-                        </select>
-                        <div className="block text-xs tracking-wider text-red-600">
-                          {errors.assignee_id && touched.assignee_id &&
-                            <div>{`${errors.assignee_id}`}</div>
-                          }
-                        </div>
-                      </div>
+        <span className="mb-3 flex justify-between font-extrabold text-base text-miru-dark-purple-1000 leading-5">
+          Candidate Settings
+          <button onClick={() => {
+            document.body.classList.remove('noscroll');
+            setShowCandidateSetting(false)
+          }}>
+            <X size={15} color="#CDD6DF" />
+          </button>
+        </span>
+        <Formik
+          initialValues={getInitialvalues(candidateDetails)}
+          enableReinitialize={true}
+          onSubmit={handleSubmit}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <div className="mb-6">
+                <label className="block mb-2 text-sm text-gray-600">Assignee</label>
+                <select
+                  defaultValue={candidateDetails.assignee_id}
+                  className="w-full px-3 py-2 bg-miru-gray-100 border border-gray-300 rounded-md  focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
+                  name="assignee_id" onChange={(e) => changeAssignee(e.target.value)}>
+                  <option value=''>Select Assignee</option>
+                  {allowUserList &&
+  allowUserList.map(e => <option value={e.id} key={e.id} selected={e.id === candidateDetails.assignee_id}>{e.first_name}{' '}{e.last_name}</option>)}
+                </select>
+                <div className="tracking-wider block text-xs text-red-600">
+                  {errors.assignee_id && touched.assignee_id &&
+            <div>{`${errors.assignee_id}`}</div>
+                  }
+                </div>
+              </div>
+              <div className="mb-6">
+                <label className="block mb-2 text-sm text-gray-600">Reporter</label>
+                <select
+                  defaultValue={candidateDetails.reporter_id}
+                  className="w-full px-3 py-2 bg-miru-gray-100 border border-gray-300 rounded-md  focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
+                  name="reporter_id" onChange={(e) => changeReporter(e.target.value)}>
+                  <option value=''>Select Reporter</option>
+                  {allowUserList &&
+  allowUserList.map(e => <option value={e.id} key={e.id} selected={e.id === candidateDetails.reporter_id}>{e.first_name}{' '}{e.last_name}</option>)}
+                </select>
+                <div className="tracking-wider block text-xs text-red-600">
+                  {errors.reporter_id && touched.reporter_id &&
+            <div>{`${errors.reporter_id}`}</div>
+                  }
+                </div>
+              </div>
 
-                      <div className="mb-6">
-                        <label className="block mb-2 text-sm text-gray-600">Reporter</label>
-                        <select
-                          defaultValue={candidateDetails.reporter_id}
-                          className="w-full px-3 py-2 border border-gray-300 rounded-md bg-miru-gray-100 focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
-                          name="reporter_id" onChange={(e) => changeReporter(e.target.value)}>
-                          <option value=''>Select Reporter</option>
-                          {allowUserList &&
-                            allowUserList.map((e: any) => <option value={e.id} selected={e.id === candidateDetails.reporter_id} key={e.id}>{e.full_name}</option>)}
-                        </select>
-                        <div className="block text-xs tracking-wider text-red-600">
-                          {errors.reporter_id && touched.reporter_id &&
-                            <div>{`${errors.reporter_id}`}</div>
-                          }
-                        </div>
-                      </div>
-
-                      <div className="mb-6">
-                        <label className="block mb-2 text-sm text-gray-600">Status</label>
-                        <select
-                          defaultValue={candidateDetails.status_code}
-                          className="w-full px-3 py-2 border border-gray-300 rounded-md bg-miru-gray-100 focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
-                          name="status_code" onChange={(e) => changeStatusCode(e.target.value)}>
-                          <option value=''>Select Status</option>
-                          {statusCodeList &&
-                            statusCodeList.map((e: any) => <option value={e.id} selected={e.id === candidateDetails.status_code} key={e.id}>{e.name}</option>)}
-                        </select>
-                        <div className="flex items-center justify-between pt-1 text-red-700">
-                          {errors.status_code && touched.status_code &&
-                            <p className="text-xs">{`${errors.status_code}`}</p>
-                          }
-                        </div>
-                      </div>
+              <div className="mb-6">
+                <label className="block mb-2 text-sm text-gray-600">Status</label>
+                <select
+                  defaultValue={candidateDetails.status_code}
+                  className="w-full px-3 py-2 bg-miru-gray-100 border border-gray-300 rounded-md  focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
+                  name="status_code" onChange={(e) => changeStatusCode(e.target.value)}>
+                  <option value=''>Select Status</option>
+                  {statusCodeList &&
+statusCodeList.map(e => <option value={e.id} key={e.id} selected={e.id === candidateDetails.status_code}>{e.name}</option>)}
+                </select>
+                <div className="flex justify-between items-center pt-1 text-red-700">
+                  {errors.status_code && touched.status_code &&
+              <p className="text-xs">{`${errors.status_code}`}</p>
+                  }
+                </div>
+              </div>
 
                     </Form>
                   )}
                 </Formik>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
     </>
   );
 };
